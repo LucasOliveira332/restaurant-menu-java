5b1b5d80e1eb7adb9308ad7d8f57a7a2202d3ca1
LucasG1020 <lucas.gomes@conforce.com.br>
2023-12-13 14:03:24 -0300

    add route get

--- RestaurantMenu/src/main/java/com/example/RestaurantMenu/Controller/FoodController.java
+++ RestaurantMenu/src/main/java/com/example/RestaurantMenu/Controller/FoodController.java
@@ -1,11 +1,24 @@
-package com.example.RestaurantMenu.Controller;
+package com.example.RestaurantMenu.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.RestaurantMenu.food.FoodRepository;
+import com.example.RestaurantMenu.food.Food;
+
 @RestController
 @RequestMapping("food")
 public class FoodController {
+  @Autowired
+  private FoodRepository repository;
   @GetMapping
+  public List<Food> Get(){
+
+    List<Food> foodList = repository.findAll();
+    return foodList;
+  }
 } 
